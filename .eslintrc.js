da4af8bf8ded75f5c4b7f45a73f26c4185f78efd
Juan Diego Macias <93182778+wasupalonely@users.noreply.github.com>
2024-09-26 11:22:29 -0500

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -22,6 +22,9 @@ module.exports = {
     },
   },
   rules: {
+    '@typescript-eslint/no-unsafe-assignment': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-floating-promises': 'off'
     '@typescript-eslint/no-unused-vars': [
       'warn',
       {
