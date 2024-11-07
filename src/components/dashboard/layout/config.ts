de76c63c0827d329c25a9f0e0e0719cd77716dd1
Calnico <calnicolas@hotmail.com>
2024-11-06 19:58:46 -0500

    Second first

--- src/components/dashboard/layout/config.ts
+++ src/components/dashboard/layout/config.ts
@@ -1,17 +1,6 @@
 import type { NavItemConfig } from '@/types/nav';
 import { paths } from '@/paths';
 
-<<<<<<< Updated upstream
-export const navItems = [
-  { key: 'home', title: 'Inicio', href: paths.dashboard.overview, icon: 'house' },
-  { key: 'customers', title: 'Usuarios', href: paths.dashboard.customers, icon: 'users' },
-  { key: 'inventory', title: 'Inventario', href: paths.dashboard.integrations, icon: 'box' },
-  // { key: 'reports', title: 'Reportes', href: paths.errors.notFound, icon: 'scroll' },
-  { key: 'account', title: 'Cuenta', href: paths.dashboard.account, icon: 'user' },
-  { key: 'supermarket', title: 'Supermercado', href: paths.dashboard.supermarket, icon: 'shopping-cart' },
-  { key: 'settings', title: 'Configuración', href: paths.dashboard.settings, icon: 'gear-six' },
-] satisfies NavItemConfig[];
-=======
 export const navItems: NavItemConfig[] = [
   { 
     key: 'home', 
@@ -67,4 +56,3 @@ export const navItems: NavItemConfig[] = [
     roles: ['admin', 'owner']
   },
 ];
->>>>>>> Stashed changes
